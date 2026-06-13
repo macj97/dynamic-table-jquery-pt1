c2cfc5b4aa1c8f000b21b351a58cea7245d378eb
Joe Plummer <jmplumm97@gmail.com>
2026-06-13 00:11:56 -0400

    edited css, html, and script.js, added script_jquery.js file

---
+++ js/script_jquery.js
@@ -0,0 +1,11 @@
+/**
+ * File: script_jquery.css
+ * GUI Assignment: HW4 - Using the jQuery Plugin/UI with Your Dynamic Table
+ * Description: JavaScript file with jQuery for Assignment 4 Part 1
+ * Joe Plummer, UMass Lowell Computer Science, joseph_plummer@student.uml.edu
+ * Copyright (c) 2026 by Joe Plummer. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
+ */
+
+$(function() {
+    $("../index.html").validate();
+});
